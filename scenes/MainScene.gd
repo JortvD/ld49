437f4e4697369c67f8b00dd3df9298e7b0b2f56e
JelteSchuurmans <j.schuurmans1999@gmail.com>
2021-10-02 10:06:10 +0200

    Working pathfinding

--- scenes/MainScene.gd
+++ scenes/MainScene.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+# this handles all the pathfinding for all entities. first if statements explains all components
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			var path = $Navigation2D.get_simple_path($NPC.position, event.position)
+		if Input.is_action_pressed("ui_1"):		#requirement for going to the position
+			var path = $Navigation2D.get_simple_path($NPC.position, Vector2(-192,-192)) # position it will go to
+			$Line2D.points = path	#TESTING PURPOSES creates line showing its path
+			$NPC.path = path		#creates the path for the entity
+		if Input.is_action_pressed("ui_2"):
+			var path = $Navigation2D.get_simple_path($NPC.position, Vector2(256,192))
 			$Line2D.points = path
 			$NPC.path = path
