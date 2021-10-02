28fb4e2217b74d1bea9c34fa416d4b9abd9c15f4
JelteSchuurmans <j.schuurmans1999@gmail.com>
2021-10-02 13:36:36 +0200

    teat

--- scenes/MainScene.gd
+++ scenes/MainScene.gd
@@ -10,3 +10,7 @@ func _unhandled_input(event):
 			var path = $Navigation2D.get_simple_path($NPC.position, Vector2(256,192))
 			$Line2D.points = path
 			$NPC.path = path
+		if Input.is_action_pressed("ui_3"):
+			var path = $Navigation2D.get_simple_path($NPC.position, $Player.position)
+			$Line2D.points = path
+			$NPC.path = path
