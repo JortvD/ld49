8273b295c1c1d154353cfa58ce131a60c92b8f9e
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2021-10-02 10:56:38 +0200

    Add more building work

--- building/Building.gd
+++ building/Building.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
 export(Texture) var texture
+export(Vector2) var entrance_location
+export(Vector2) var entrance_shape
 
 func _ready():
 	$Sprite.texture = texture
 	var size = texture.get_size() / 2
 	$StaticBody2D/CollisionShape2D.shape.extents = size
+	$Area2D.position = entrance_location
+	$Area2D/CollisionShape2D.shape.extents = entrance_shape
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		print("test")
