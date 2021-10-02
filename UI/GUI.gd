4e5881ac48e89f212fa30244754e0dc975e43e90
JelteSchuurmans <j.schuurmans1999@gmail.com>
2021-10-02 21:28:48 +0200

    genius jort

--- UI/GUI.gd
+++ UI/GUI.gd
@@ -1,7 +1,9 @@
 extends Control
 
+
 func _process(delta):
-	$Panel/Sprite.texture = load($"/root/MainScene/Player".inventory[0].img)
+	
+	$Panel/Sprite.texture = load($"/root/MainScene/Player".inventory[0].img) 
 	$Panel/Sprite.texture = load($"/root/MainScene/Player".inventory[1].img)
 	$Panel/Sprite.texture = load($"/root/MainScene/Player".inventory[2].img)
 	$Panel/Sprite.texture = load($"/root/MainScene/Player".inventory[3].img)
